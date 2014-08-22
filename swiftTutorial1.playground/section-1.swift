02593d316b1b7138647eb2a08bfc6dee84c66cbe
55011212169_Ralintorn <ralintorn.gle@gmail.com>
2014-08-22 14:02:56 +0700

    fdsfsdf
    fdsfsdf

--- swiftTutorial1.playground/section-1.swift
+++ swiftTutorial1.playground/section-1.swift
@@ -7,3 +7,36 @@ let tutorialTeam = 56
 let editorialTeam = 23
 var totalTeam = tutorialTeam + editorialTeam
 totalTeam+=1
+let priceIntInferred = -19
+let princeIntExplicit:Int = -19
+
+let priceIferred = -19.99
+let priceExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+var shoppingListExplicit = [String]()
+shoppingListExplicit = ["Eggs","Milk"]
+//var shoppingExplicit: [String] = ["Eggs","Milk"]
+
+var shoppingList = ["Eggs","Milk"]
+println("The shopping list contains \(shoppingList.count) items.")
+shoppingList.append("Flour")
+shoppingList += "Baking Powder"
+shoppingList += ["Choccolate Spread","Cheess","Butter"]
+var firstItem = shoppingList[0]
+shoppingList[0] = "Six Eggs"
+//shoppingList[0...3] = ["Bananas","Apples"]
+shoppingList
+
+
+
+
+
